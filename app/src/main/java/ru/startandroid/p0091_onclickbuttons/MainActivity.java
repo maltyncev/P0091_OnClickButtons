6b541be5d42502c6e2ae7ae4035b74c3f43285ef
maltyncev <maltyncev@gmail.com>
2018-11-23 11:32:32 +0500

    Initial commit

---
+++ app/src/main/java/ru/startandroid/p0091_onclickbuttons/MainActivity.java
@@ -0,0 +1,49 @@
+package ru.startandroid.p0091_onclickbuttons;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvOut;
+    Button btnOk;
+    Button btnCancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // найдем View-элементы
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        btnOk = (Button) findViewById(R.id.btnOk);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+
+        // создаем обработчик нажатия
+        final View.OnClickListener oclBtnOk = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Меняем текст в TextView (tvOut)
+                tvOut.setText("Нажата кнопка OK");
+            }
+        };
+
+        // присвоим обработчик кнопке OK (btnOk)
+        btnOk.setOnClickListener(oclBtnOk);
+
+        // создаем обработчик нажатия
+        final View.OnClickListener oclbtnCancel = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Меняем текст в TextView (tvOut)
+                tvOut.setText("Нажата кнопка Cancel");
+            }
+        };
+
+        // присвоим обработчик кнопке OK (btnOk)
+        btnCancel.setOnClickListener(oclbtnCancel);
+    }
+}
